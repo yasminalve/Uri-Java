2153e6d2a0357c689c2d1b101fcc968d8af38695
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-25 20:21:49 -0300

    Create 1001.java

---
+++ 1001.java
@@ -0,0 +1,25 @@
+//Yasmin Alves
+//25.08.2020
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        int a, b, c;
+        
+        Scanner in = new Scanner (System.in);
+        
+        a = in.nextInt();
+        b = in.nextInt();
+        c = a + b;
+        
+        System.out.println("X = " + c);
+        
+        
+ 
+    }
+ 
+}
